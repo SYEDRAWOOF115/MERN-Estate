80906820b9f90f3ed61014126385311f4f82751e
syedrawoof <syedrawoof115@gmail.com>
2024-04-15 00:51:16 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import express from 'express';
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("connected to MongoDB!");
+})
+.catch((err)=>{
+    console.log(err);
+})
 const app = express();
 
 app.listen(3000,()=>{
