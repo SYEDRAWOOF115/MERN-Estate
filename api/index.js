398be1cbaeee2bd5f5f40d11d6f1d2aee4cc85ff
syedrawoof <syedrawoof115@gmail.com>
2024-04-15 17:44:50 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import express from 'express';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js';
 
 dotenv.config();
 
@@ -18,7 +19,7 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-
+app.use('/api/listing', listingRouter);
 
 
 app.listen(3000,()=>{
