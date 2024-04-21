8329b3642c70aadfcdde4210b75bf152b49bddcd
syedrawoof <syedrawoof115@gmail.com>
2024-04-22 00:18:05 +0530

    signout problem corrected

--- api/index.js
+++ api/index.js
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import express from 'express';
-
 import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js'
+import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
 import path from 'path';
@@ -11,38 +10,112 @@ import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
-.then(()=>{
-    console.log("connected to MongoDB!");
-})
-.catch((err)=>{
-    console.log(err);
-});
+    .then(() => {
+        console.log("Connected to MongoDB!");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 const __dirname = path.resolve();
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/listing', listingRouter);
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
-
-
-app.listen(3000,()=>{
-    console.log('server is running on port 3000!');
+// Define your "/" route before wildcard route
+app.get('/', (req, res) => {
+    res.send('hello world');
 });
 
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
+// Wildcard route should be defined at the end
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
+        success: false,
+        statusCode,
+        message,
     });
-  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import express from 'express';
+
+// import userRouter from './routes/user.route.js';
+// import authRouter from './routes/auth.route.js'
+// import cookieParser from 'cookie-parser';
+// import listingRouter from './routes/listing.route.js';
+// import path from 'path';
+
+// dotenv.config();
+
+// mongoose.connect(process.env.MONGO)
+// .then(()=>{
+//     console.log("connected to MongoDB!");
+// })
+// .catch((err)=>{
+//     console.log(err);
+// });
+// const __dirname = path.resolve();
+// const app = express();
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use('/api/listing', listingRouter);
+// app.use(express.static(path.join(__dirname, '/client/dist')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// })
+
+
+// app.listen(3000,()=>{
+//     console.log('server is running on port 3000!');
+// });
+
+
+// app.use('/api/user',userRouter);
+// app.use('/api/auth',authRouter);
+
+// app.use((err, req, res, next) => {
+//     const statusCode = err.statusCode || 500;
+//     const message = err.message || 'Internal Server Error';
+//     return res.status(statusCode).json({
+//       success: false,
+//       statusCode,
+//       message,
+//     });
+//   });
